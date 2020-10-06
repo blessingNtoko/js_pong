83c2ceee60852a4228341420c612104ea70022a0
Blessing <bntoko@gmail.com>
2020-10-06 17:05:21 +0200

    added classes | added planned functions

---
+++ pong.js
@@ -0,0 +1,77 @@
+let canvas;
+let ctx;
+
+let DIRECTION = {
+    STOPPED: 0,
+    UP: 1,
+    DOWN: 2,
+    LEFT: 3,
+    RIGHT: 4,
+}
+
+class Paddle {
+    constructor(side) {
+        this.width = 15;
+        this.height = 65;
+        this.x = side === 'left' ? 150 : canvas.width - 150;
+        this.y = canvas.height / 2;
+        this.score = 0;
+        this.move = DIRECTION.STOPPED;
+        this.speed = 11;
+    }
+}
+
+class Ball {
+    constructor(newSpeed) {
+        this.width = 15;
+        this.height = 15;
+        this.x = canvas.width / 2;
+        this.y = canvas.height / 2;
+        this.moveX = DIRECTION.STOPPED;
+        this.moveY = DIRECTION.STOPPED;
+        this.speed = newSpeed;
+    }
+}
+
+let player;
+let aiPlayer;
+let ball;
+let running = false;
+let gameOver = false;
+let delayAmount;
+let targetForBall;
+let beepSound;
+
+document.addEventListener("DOMContentLoaded", setupCanvas);
+
+function setupCanvas() {
+
+}
+
+function drawOnCanvas() {
+
+}
+
+function updateCanvas() {
+
+}
+
+function movePlayerPaddle() {
+
+}
+
+function stopPlayerPaddle() {
+
+}
+
+function resetBall() {
+
+}
+
+function gameLoop() {
+
+}
+
+function addDelay() {
+    
+}
